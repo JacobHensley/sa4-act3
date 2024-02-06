37e890538fcfb8b17019189a8400a5aa7982afee
Jacob Hensley <jacobdlhensley@gmail.com>
2024-02-06 11:55:53 -0600

    Added option for user to exit

--- guess_number.py
+++ guess_number.py
@@ -1,8 +1,13 @@
 number = 10
 print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of? "))
 
-if guess == number:
-    print("Congratulations! You guessed the right number.")
-else:
-    print(f"Sorry! The number was {number}.")
+while True:
+    guess = input("What number am I thinking of? ")
+    if guess == 'q':
+        print(f"Sorry! The number was {number}.")
+        break
+    elif int(guess) == number:
+        print("Congratulations! You guessed the right number.")
+        break
+    else:
+        print("Try again! or type q to exit.")
